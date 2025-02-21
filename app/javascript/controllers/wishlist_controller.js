ec6d26da29bc9ccb8061eebe8e5e5b9d6ca1b9eb
Livia Calderan <liv_calderan@hotmail.com>
2025-02-21 09:57:24 -0400

    add wishlist

---
+++ app/javascript/controllers/wishlist_controller.js
@@ -0,0 +1,80 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+
+    
+    updateWishlistStatus() { 
+        
+        const isUserLoggedIn = this.element.dataset.userLoggedIn;
+    
+        if(isUserLoggedIn === "false" ) {
+            document.querySelector(".js-login").click();
+            return 
+        }
+
+        if (this.element.dataset.status === "false") {
+
+            const propertyId = this.element.dataset.propertyId;
+            const userId = this.element.dataset.userId;
+
+            console.log("Property ID:", propertyId);
+            console.log("User ID:", userId);
+
+            this.addPropertyToWishlist(propertyId, userId)
+        } else {
+
+            const wishlistId =this.element.dataset.wishlistId;
+            this.removePropertyFromWishlist(wishlistId)
+        }
+        
+    }
+
+    addPropertyToWishlist(propertyId, userId) {
+        const params = {
+            property_id: propertyId,
+            user_id: userId,
+        };
+
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(params),
+        };
+
+        fetch( '/api/wishlists', options)
+        .then(response => {
+
+            if(!response.ok) {
+                console.log(response.status);
+            }
+            return response.json();
+        })
+        .then(data => {
+            this.element.dataset.wishlistId = data.id;
+            this.element.classList.remove("fill-none");
+            this.element.classList.add("fill-[#FF5A5F]");
+            this.element.dataset.status = "true";
+        })
+        .catch(e => {
+            console.log(e);
+        });
+    }
+
+    removePropertyFromWishlist(wishlistId) {
+        console.log("Removendo da wishlist, ID:", wishlistId);
+        fetch('/api/wishlists/' + wishlistId, {
+            method: 'DELETE',
+        })
+        .then(response => {
+            this.element.dataset.wishlistId = '';
+            this.element.classList.remove("fill-[#FF5A5F]");
+            this.element.classList.add("fill-none");
+            this.element.dataset.status = "false";
+        })
+        .catch(e => {
+            console.log(e);
+        });
+    }
+}
